03c5fbaf20c1ce4f4caf0a25278d37b96bdd9035
benditsm <ross@benditsm.com>
2026-03-26 17:23:06 -0700

    remove fix for GH pages css problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'https://benditsm.github.io/benditsmweb-a-g-n/', //absolute path for github pages
+  
   vite: {
     plugins: [tailwindcss()]
-  },
-  build: {outDir: 'dist'}
+  }
 });
